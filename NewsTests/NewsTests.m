907083378e13324e86ea76e3c8f100746d0270d1
karta sutanto <karta022@yahoo.com.sg>
2013-07-01 13:34:19 +0800

    Initial Commit

---
+++ NewsTests/NewsTests.m
@@ -0,0 +1,36 @@
+//
+//  NewsTests.m
+//  NewsTests
+//
+//  Created by karta sutanto on 1/7/13.
+//  Copyright (c) 2013 karta sutanto. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NewsTests : XCTestCase
+
+@end
+
+@implementation NewsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

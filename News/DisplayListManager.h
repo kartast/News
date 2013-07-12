0ab7b496725cca96f8f477a66eed53605c7337d0
karta sutanto <karta022@yahoo.com.sg>
2013-07-12 09:57:21 +0800

    added displaylistmanager

---
+++ News/DisplayListManager.h
@@ -0,0 +1,14 @@
+//
+//  DisplayListManager.h
+//  News
+//
+//  Created by karta sutanto on 12/7/13.
+//  Copyright (c) 2013 karta sutanto. All rights reserved.
+//
+//  This singleton class objective is to manage the displayList
+
+#import <Foundation/Foundation.h>
+
+@interface DisplayListManager : NSObject
+
+@end
---
+++ News/DisplayListManager.m
@@ -0,0 +1,13 @@
+//
+//  DisplayListManager.m
+//  News
+//
+//  Created by karta sutanto on 12/7/13.
+//  Copyright (c) 2013 karta sutanto. All rights reserved.
+//
+
+#import "DisplayListManager.h"
+
+@implementation DisplayListManager
+
+@end
